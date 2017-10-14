e8e2cc165b16056c4fda56534e23dda2b1afe39b
Felix Jones <felix@felixjones.co.uk>
2017-10-14 20:14:45 +0100

    Reduced precision (Fixes move routes taking too long)

--- movement/js/plugins/AltimitMovement.js
+++ movement/js/plugins/AltimitMovement.js
@@ -1727,7 +1727,7 @@
     Collider.CIRCLE = 0;
     Collider.POLYGON = 1;
     Collider.LIST = 2;
-    Collider.PRECISION = Math.pow( 2, 16 );
+    Collider.PRECISION = Math.pow( 2, 8 );
     Collider.I_PRECISION = 1 / Collider.PRECISION;
 
     Collider.createList = function() {
