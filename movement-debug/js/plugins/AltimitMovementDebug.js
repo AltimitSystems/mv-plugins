49f60c89e3553ac45497d0c14d44ce27c946aff1
Felix Jones <fewix3000@hotmail.co.uk>
2017-10-29 13:16:16 +0000

    Changed copyright to Community Contributors

--- movement-debug/js/plugins/AltimitMovementDebug.js
+++ movement-debug/js/plugins/AltimitMovementDebug.js
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2017 Altimit Systems LTD
+ * Copyright (c) 2017 Altimit Community Contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
